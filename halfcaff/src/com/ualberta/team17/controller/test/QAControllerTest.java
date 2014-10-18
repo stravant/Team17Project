16e916963f591e836fa9b23574487b1d4d7c1b7f
Corey Jamison <cjamison@ualberta.ca>
2014-10-18 00:37:11 -0600

    Added unit tests for QAController and IncrementalResults. Also created dummy classes that made them easier to test

---
+++ halfcaff/src/com/ualberta/team17/controller/test/QAControllerTest.java
@@ -0,0 +1,59 @@
+package com.ualberta.team17.controller.test;
+
+import com.ualberta.team17.AnswerItem;
+import com.ualberta.team17.AttachmentItem;
+import com.ualberta.team17.CommentItem;
+import com.ualberta.team17.ItemType;
+import com.ualberta.team17.QuestionItem;
+import com.ualberta.team17.controller.QAController;
+import com.ualberta.team17.datamanager.DataFilter;
+import com.ualberta.team17.datamanager.DataManager;
+import com.ualberta.team17.datamanager.IncrementalResult;
+import com.ualberta.team17.datamanager.comparators.UpvoteComparator;
+
+import junit.framework.TestCase;
+
+public class QAControllerTest extends TestCase {
+	private QAController controller;
+	private DummyDataManager dataManager;
+	private QuestionItem parentQuestion;
+	
+	public void SetUp() {
+		dataManager = new DummyDataManager();
+		controller = new QAController( dataManager );
+		parentQuestion = new QuestionItem(null, null, null, null, null, 0, null);
+	}
+	
+	public void Test_QAC1_GetObjects() {
+		IncrementalResult result = controller.getObjects(new DataFilter(){}, new UpvoteComparator() );
+		
+		assertEquals("getObjects", result.getCurrentResultCount(), dataManager.getItemCount() );
+	}
+	
+	public void Test_QAC2_GetQuestionChildren() {
+		IncrementalResult result = controller.getObjects(new DataFilter(){}, new UpvoteComparator() );
+		QuestionItem question = (QuestionItem) result.getCurrentResultsOfType(ItemType.Question).get(0);
+		
+		IncrementalResult childrenResult = controller.getQuestionChildren( question, new UpvoteComparator() );
+	}
+	
+	public void Test_QAC3_CreateQuestion() {
+		QuestionItem question = controller.createQuestion( "title", "body" );
+		assertNotNull( question );
+	}
+	
+	public void Test_QAC4_CreateAttachment() {
+		AttachmentItem attachment = controller.createAttachment( parentQuestion );
+		assertNotNull( attachment );
+	}
+	
+	public void Test_QAC5_CreateAnswer() {
+		AnswerItem answer = controller.createAnswer(parentQuestion, "answer body" );
+		assertNotNull( answer );
+	}
+	
+	public void Test_QAC6_CreateComment() {
+		CommentItem comment = controller.createComment( parentQuestion, "comment body" );
+		assertNotNull( comment );
+	}
+}
