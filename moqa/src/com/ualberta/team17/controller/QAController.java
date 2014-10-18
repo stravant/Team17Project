94df6666b94b1d3f177952ce22bd2d163f39490e
Mobius5150 <mike@michaelblouin.ca>
2014-10-18 14:36:00 -0600

    Stubbing QAController.
    Throwing `UnsupportedOperationExceptions` in unimplemented methods.
    
    See #29

--- moqa/src/com/ualberta/team17/controller/QAController.java
+++ moqa/src/com/ualberta/team17/controller/QAController.java
@@ -5,7 +5,9 @@ import com.ualberta.team17.AttachmentItem;
 import com.ualberta.team17.AuthoredTextItem;
 import com.ualberta.team17.CommentItem;
 import com.ualberta.team17.QuestionItem;
+import com.ualberta.team17.datamanager.DataFilter;
 import com.ualberta.team17.datamanager.DataManager;
+import com.ualberta.team17.datamanager.IItemComparator;
 import com.ualberta.team17.datamanager.IncrementalResult;
 
 public class QAController {
@@ -16,23 +18,23 @@ public class QAController {
 	
 	/* Access existing content */
 	public IncrementalResult getObjects(DataFilter filter, IItemComparator sort) {
-		
+		throw new UnsupportedOperationException();
 	}
 	public IncrementalResult getQuestionChildren(QuestionItem question, IItemComparator sort) {
-		
+		throw new UnsupportedOperationException();
 	}
 	
 	/* Create new content */
 	public QuestionItem createQuestion(String title, String body) {
-		
+		throw new UnsupportedOperationException();
 	}
 	public AttachmentItem createAttachment(QuestionItem parent) {
-		
+		throw new UnsupportedOperationException();
 	}
 	public AnswerItem createAnswer(QuestionItem parent, String body) {
-		
+		throw new UnsupportedOperationException();
 	}
 	public CommentItem createComment(AuthoredTextItem parent, String body) {
-		
+		throw new UnsupportedOperationException();
 	}
 }
