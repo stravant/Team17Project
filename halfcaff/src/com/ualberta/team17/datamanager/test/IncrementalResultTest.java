6ed7594eff80d3a861183c04b98974615f6b10ca
Mark Langen <stravant@gmail.com>
2014-10-24 02:00:26 -0600

    IncrementalResult Implemented, with API modified a bit to better reflect it's asynchronous operation. Needs a few more tests, but it looks like it works.

--- halfcaff/src/com/ualberta/team17/datamanager/test/IncrementalResultTest.java
+++ halfcaff/src/com/ualberta/team17/datamanager/test/IncrementalResultTest.java
@@ -10,53 +10,100 @@ import com.ualberta.team17.QuestionItem;
 import com.ualberta.team17.UniqueId;
 import com.ualberta.team17.datamanager.IncrementalResult;
 import com.ualberta.team17.datamanager.comparators.UpvoteComparator;
+import com.ualberta.team17.test.utility.OrderedQAItem;
+import com.ualberta.team17.test.utility.OrderedQAItemComparator;
 
 import junit.framework.TestCase;
 
 public class IncrementalResultTest extends TestCase {
 	private IncrementalResult result;
+	private RecordingIncrementalObserver obs;
 	public void setUp() {
-		result = new IncrementalResult( new UpvoteComparator() );
-	}
-	
-	public void test_IR1_ObserverTest() {
-		DummyIncrementalObserver obs = new DummyIncrementalObserver();
-		
+		result = new IncrementalResult(new OrderedQAItemComparator());
+		obs = new RecordingIncrementalObserver();
 		result.addObserver(obs);
-		result.addObjects(new ArrayList<QAModel>() );
-		
-		assertTrue( "Observer notified", obs.wasNotified() );
 	}
 	
-	public void test_IR2_GetCurrentResultCount() {
-		assertEquals( "No results initially", result.getCurrentResultCount(), 0 );
+	@SuppressWarnings("serial")
+	public void test_IR1_BasicObjectInsertion() {
+		result.addObjects(new ArrayList<QAModel>(){{}});
 		
-		List<QAModel> itemsToAdd = new ArrayList<QAModel>();
-		itemsToAdd.add(new QuestionItem(new UniqueId(), null, null, null, null, 0, null));
+		assertFalse("Empty list of items to add, should not be notified", obs.wasNotified());
 		
-		result.addObjects(itemsToAdd);
+		result.addObjects(new ArrayList<QAModel>(){{
+			add(new OrderedQAItem(ItemType.Question, 0));
+		}});
 		
-		assertEquals( "One result after adding", result.getCurrentResultCount(), 1);
+		assertTrue("Item was added", obs.wasNotified());
 	}
 	
-	public void test_IR3_GetCurrentResult() {
-		QuestionItem item1 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
-		QuestionItem item2 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
+	@SuppressWarnings("serial")
+	public void test_IR2_TestNotificationCount() {
+		result.addObjects(new ArrayList<QAModel>(){{
+			add(new OrderedQAItem(ItemType.Question, 0));
+			add(new OrderedQAItem(ItemType.Question, 1));
+		}});
 		
-		List<QAModel> itemsToAdd = new ArrayList<QAModel>();
-		itemsToAdd.add(item1);
-		itemsToAdd.add(item2);
+		assertEquals("Two items added -> one notification", obs.getNotificationCount(), 1);
 		
-		result.addObjects(itemsToAdd);
+		result.addObjects(new ArrayList<QAModel>(){{
+			add(new OrderedQAItem(ItemType.Answer, 2));
+		}});
 		
-		assertEquals( "First result", result.getCurrentResult(0), item1);
-		assertEquals( "Second result", result.getCurrentResult(1), item2);
+		assertEquals("One further item added -> one further notification", obs.getNotificationCount(), 2);
 	}
+	
+	public void test_IR3_OutOfOrderInsert() {
+		final OrderedQAItem q1 = new OrderedQAItem(ItemType.Question, 0);
+		final OrderedQAItem q2 = new OrderedQAItem(ItemType.Question, 1);
+		final OrderedQAItem q3 = new OrderedQAItem(ItemType.Question, 2);
+		
+		result.addObjects(new ArrayList<QAModel>(){{
+			add(q1);
+			add(q3);
+			add(q2);
+		}});
+		
+		assertEquals("Thee items added with last out of order -> two notifications", obs.getNotificationCount(), 2);
+		
+		assertEquals(0, obs.getNotification(0).Index);
+		assertEquals(2, obs.getNotification(0).List.size());
+		assertEquals(q1, obs.getNotification(0).List.get(0));
+		assertEquals(q3, obs.getNotification(0).List.get(1));
+		assertEquals(1, obs.getNotification(1).Index);
+		assertEquals(1, obs.getNotification(1).List.size());
+		assertEquals(q2, obs.getNotification(1).List.get(0));
+	}
+	
+//	public void test_IR2_GetCurrentResultCount() {
+//		assertEquals( "No results initially", result.getCurrentResultCount(), 0 );
+//		
+//		List<QAModel> itemsToAdd = new ArrayList<QAModel>();
+//		itemsToAdd.add(new QuestionItem(new UniqueId(), null, null, null, null, 0, null));
+//		
+//		result.addObjects(itemsToAdd);
+//		
+//		assertEquals( "One result after adding", result.getCurrentResultCount(), 1);
+//	}
+	
+//	public void test_IR3_GetCurrentResult() {
+//		QuestionItem item1 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
+//		QuestionItem item2 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
+//		
+//		List<QAModel> itemsToAdd = new ArrayList<QAModel>();
+//		itemsToAdd.add(item1);
+//		itemsToAdd.add(item2);
+//		
+//		result.addObjects(itemsToAdd);
+//		
+//		assertEquals("First result", result.getCurrentResult(0), item1);
+//		assertEquals("Second result", result.getCurrentResult(1), item2);
+//	}
 	
 	public void test_IR4_GetCurrentResultsOfType() {
-		QuestionItem item1 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
-		AnswerItem item2 = new AnswerItem(new UniqueId(), null, null, null, null, 0);
-		QuestionItem item3 = new QuestionItem(new UniqueId(), null, null, null, null, 0, null);
+		OrderedQAItem item1 = new OrderedQAItem(ItemType.Question, 0);
+		OrderedQAItem item2 = new OrderedQAItem(ItemType.Answer,   1);
+		OrderedQAItem item3 = new OrderedQAItem(ItemType.Question, 2);
 		
 		List<QAModel> itemsToAdd = new ArrayList<QAModel>();
 		itemsToAdd.add(item1);
--- moqa/src/com/ualberta/team17/datamanager/IncrementalResult.java
+++ moqa/src/com/ualberta/team17/datamanager/IncrementalResult.java
@@ -1,6 +1,11 @@
 package com.ualberta.team17.datamanager;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+import android.util.Log;
 
 import com.ualberta.team17.QAModel;
 import com.ualberta.team17.ItemType;
@@ -12,40 +17,165 @@ import com.ualberta.team17.ItemType;
  * be notified.
  */
 public class IncrementalResult {
-	// Note: We can't just use synchronized willy-nilly here when there is observer pattern
-	// involved, since we can end up with deadlocks of:
-	// addObject() --> notifies --> notified object calls getCurrentResults() --> this is still locked, deadlock
-	// Finer grained locking control is needed.
+	/* ReentrantLock to avoid problems with an Observer calling back 
+	 * to getCurrentResult/s within the notify call.
+	 * Possibly can just be a normal lock.
+	 */
+	private ReentrantLock mModifyMutex = new ReentrantLock();
 	
+	// The list of items that have been added to this result.
+	private ArrayList<QAModel> mResultList = new ArrayList<QAModel>();
+	
+	// Comparator to use on the items when inserting them.
+	private IItemComparator mSort;
+	
+	/*
+	 * Internal class to attach a desired item type to be notified on to a
+	 * IIncrementalObserver.
+	 */
+	private class ObserverEntry {
+		private IIncrementalObserver mObserver;
+		private ItemType mTargetType; // null -> accept any
+		
+		// Construct with a filter
+		public ObserverEntry(IIncrementalObserver observer, ItemType targetType) {
+			mObserver = observer;
+			mTargetType = targetType;
+		}
+		
+		// Construct without a type filter
+		public ObserverEntry(IIncrementalObserver observer) {
+			this(observer, null);
+		}
+		
+		// Try to notify the observer
+		public void tryNotify(List<QAModel> entries, int index) {
+			if (mTargetType != null) {
+				// If we have a type of interest, then we have to notify on
+				// The view of this result as a list of that type only.
+				
+				// Shift the index over to be an index into the list of only items
+				// of the type we're interested in
+				for (int i = 0; i < index; ++i)
+					if (mResultList.get(i).getItemType() != mTargetType)
+						--index;
+				
+				// Create a list of only the added entries of the type we want
+				List<QAModel> notifyList = new ArrayList<QAModel>();
+				for (QAModel entry: entries) {
+					if (entry.getItemType() == mTargetType) {
+						notifyList.add(entry);
+					}
+				}
+				
+				// Notify on that filtered list
+				mObserver.itemsArrived(notifyList, index);
+			} else {
+				// Otherwise, just notify on the entries that we got
+				mObserver.itemsArrived(entries, index);
+			}
+		}
+	}
+	
+	// List of observers
+	private ArrayList<ObserverEntry> mObserverList = new ArrayList<ObserverEntry>();
+	
+	// Constructor
 	public IncrementalResult(IItemComparator sort) {
-		throw new UnsupportedOperationException();
+		mSort = sort;
 	}
 	
+	// Helper, used by addObserver
+	private void notifyOfCurrentResults(ObserverEntry entry) {
+		if (!mResultList.isEmpty())
+			entry.tryNotify(mResultList, 0);
+	}
+	
+	/*
+	 * Adds an observer, and calls it's notify method immediately for
+	 * every currently present result.
+	 */
 	public void addObserver(IIncrementalObserver observer) {
-		throw new UnsupportedOperationException();
+		mModifyMutex.lock();
+		ObserverEntry entry = new ObserverEntry(observer);
+		mObserverList.add(entry);
+		notifyOfCurrentResults(entry);
+		mModifyMutex.unlock();
+	}
+	public void addObserver(IIncrementalObserver observer, ItemType type) {
+		mModifyMutex.lock();
+		ObserverEntry entry = new ObserverEntry(observer, type);
+		mObserverList.add(entry);
+		notifyOfCurrentResults(entry);
+		mModifyMutex.unlock();
 	}
 	
-	public void addObserver(IIncrementalObserver observer, ItemType type) {
-		throw new UnsupportedOperationException();
+	// Notify the observers that a(n) item(s) arrived.
+	private void notifyObservers(List<QAModel> objects, int atIndex) {
+		for (ObserverEntry entry: mObserverList) {
+			entry.tryNotify(objects, atIndex);
+		}
 	}
 	
 	public void addObjects(List<QAModel> objects) {
-		throw new UnsupportedOperationException();
+		mModifyMutex.lock();
+		
+		// For each model in the things to add, add it to the list in a sorted position
+		// Try to notify in chunks of multiple items where possible (when multiple items
+		// are added to consecutive positions in the list)
+		
+		// Set up tracking for blocks of inserted entries to notify on
+		int lastInsertAt = -2;
+		int insertListStart = -2;
+		ArrayList<QAModel> notifyList = new ArrayList<QAModel>();
+		
+		// For each item to add
+		for (QAModel model: objects) {
+			// Find out where to add it via binary search
+			int foundIndex = Collections.binarySearch(mResultList, model, mSort);
+			//Log.i("test", "FoundIndex: " + foundIndex);
+			if (foundIndex >= 0) throw new AssertionError("Attempt to double-insert an item into an IncrementalResult");
+			int insertIndex = (-foundIndex) - 1;
+			
+			// If it follows the last item inserted in a logical block, then add it to the notify list
+			if (insertIndex == lastInsertAt + 1) {
+				++lastInsertAt;
+				notifyList.add(model);
+			} else {
+				// Otherwise, notify on the current notify list, and start a new notify list with
+				// the current item in it.
+				if (!notifyList.isEmpty())
+					notifyObservers(notifyList, insertListStart);
+				notifyList.clear();
+				notifyList.add(model);
+				lastInsertAt = insertIndex;
+				insertListStart = lastInsertAt;
+			}
+			mResultList.add(insertIndex, model);
+		}
+		
+		// Notify on the remaining notify list if there's any remaining items
+		if (!notifyList.isEmpty())
+			notifyObservers(notifyList, insertListStart);
+		
+		mModifyMutex.unlock();
 	}
 	
-	public int getCurrentResultCount() {
-		throw new UnsupportedOperationException();
-	}
-	
-	public QAModel getCurrentResult(int index) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public List<QAModel> getCurrentResults() {
-		throw new UnsupportedOperationException();
-	}
-	
+	// Get a list of the current results of the given type
 	public List<QAModel> getCurrentResultsOfType(ItemType type) {
-		throw new UnsupportedOperationException();
+		mModifyMutex.lock();
+		ArrayList<QAModel> results = new ArrayList<QAModel>();
+		for (QAModel result: mResultList) {
+			if (result.getItemType() == type) {
+				results.add(result);
+			}
+		}
+		mModifyMutex.unlock();
+		return results;
+	}
+	
+	// Gets a copy of the current result list
+	public List<QAModel> getCurrentResults() {
+		return new ArrayList<QAModel>(mResultList);
 	}
 }
