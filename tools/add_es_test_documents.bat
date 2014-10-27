85a3d02edbfc401adbd80d772b76f1c4e75a2dba
Mobius5150 <mike@michaelblouin.ca>
2014-10-27 14:23:06 -0600

    Updating names of stock test ES elements.
    Updating the field names in the elements used in automated test cases against the ES test server.

--- tools/add_es_test_documents.bat
+++ tools/add_es_test_documents.bat
@@ -1,5 +1,5 @@
-curl -XDELETE "http://192.168.0.23:9200/moqatest"
-curl -XPUT "http://192.168.0.23:9200/moqatest"
-curl -XPOST "http://192.168.0.23:9200/_bulk?pretty" --data-binary @bulk_questions.txt
-curl -XPOST "http://192.168.0.23:9200/_bulk?pretty" --data-binary @bulk_answers.txt
-curl -XPOST "http://192.168.0.23:9200/_bulk?pretty" --data-binary @bulk_comments.txt
+curl -XDELETE "http://68.149.53.165:9182/moqatest"
+curl -XPUT "http://68.149.53.165:9182/moqatest"
+curl -XPOST "http://68.149.53.165:9182/_bulk?pretty" --data-binary @bulk_questions.txt
+curl -XPOST "http://68.149.53.165:9182/_bulk?pretty" --data-binary @bulk_answers.txt
+curl -XPOST "http://68.149.53.165:9182/_bulk?pretty" --data-binary @bulk_comments.txt
