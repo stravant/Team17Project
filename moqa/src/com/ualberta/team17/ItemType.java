ab44faeff1437c8ebd6260e144fdc5092edb8fe0
Mobius5150 <mike@michaelblouin.ca>
2014-10-27 14:37:42 -0600

    Adding method to get item type enum from string.

--- moqa/src/com/ualberta/team17/ItemType.java
+++ moqa/src/com/ualberta/team17/ItemType.java
@@ -5,5 +5,27 @@ public enum ItemType {
 	Attachment,
 	Comment,
 	Question,
-	Answer
+	Answer;
+
+	public static ItemType fromString(String text) {
+		for (ItemType type : ItemType.values()) {
+			if (text.equalsIgnoreCase(type.toString())) {
+				return type;
+			}
+		}
+
+		if (text.equalsIgnoreCase("Question")) {
+			return Question;
+		} else if (text.equalsIgnoreCase("Answer")) {
+			return Answer;
+		} else if (text.equalsIgnoreCase("Comment")) {
+			return Comment;
+		} else if (text.equalsIgnoreCase("Attachment")) {
+			return Attachment;
+		} else if (text.equalsIgnoreCase("Upvote")) {
+			return Upvote;
+		}
+
+		return null;
+	}
 }
