f031b4264ea0031fd96f5984726a52947f9be17d
Mobius5150 <mike@michaelblouin.ca>
2014-10-31 11:45:22 -0600

    Add Sort Direction support to comparator interfaces.
    Comparators can now have the sort direction specified to be `Ascending` or `Descending`.
    
    Also added a `getFilterField()` function that returns the name of the field that the comparator sorts on.
    
    Closes #72

--- moqa/src/com/ualberta/team17/datamanager/comparators/UpvoteComparator.java
+++ moqa/src/com/ualberta/team17/datamanager/comparators/UpvoteComparator.java
@@ -1,14 +1,50 @@
 package com.ualberta.team17.datamanager.comparators;
 
+import com.ualberta.team17.AuthoredTextItem;
 import com.ualberta.team17.QAModel;
 import com.ualberta.team17.datamanager.IItemComparator;
 
 public class UpvoteComparator implements IItemComparator {
+	private SortDirection mCompareDirection = SortDirection.Ascending;
 
 	@Override
 	public int compare(QAModel lhs, QAModel rhs) {
-		// TODO Auto-generated method stub
+		return
+			(mCompareDirection == SortDirection.Ascending ? 1 : -1)
+			* directionalCompare(lhs, rhs);
+	}
+
+	private int directionalCompare(QAModel lhs, QAModel rhs) {
+		if (null == lhs) {
+			return -1;
+		}
+
+		if (null == rhs) {
+			return 1;
+		}
+
+		if ((lhs instanceof AuthoredTextItem) && (rhs instanceof AuthoredTextItem)) {
+			AuthoredTextItem left = (AuthoredTextItem)lhs;
+			AuthoredTextItem right = (AuthoredTextItem)rhs;
+
+			return Integer.valueOf(left.getUpvoteCount()).compareTo(right.getUpvoteCount());
+		}
+
 		return 0;
 	}
 
+	@Override
+	public void setCompareDirection(SortDirection direction) {
+		mCompareDirection = direction;
+	}
+
+	@Override
+	public SortDirection getCompareDirection() {
+		return mCompareDirection;
+	}
+
+	@Override
+	public String getFilterField() {
+		return "<num_upvotes>";
+	}
 }
