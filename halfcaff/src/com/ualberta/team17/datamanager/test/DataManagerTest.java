15904d52b92859c64ce8e5186f158e2eaccbc98e
Mark Langen <stravant@gmail.com>
2014-11-05 23:43:58 -0700

    Changed LocalDataManager to only use Context, but no UserContext. Done by moving the local filesystem saving information into static methods in the DataManager rather than being tied to a UserContext. Made UserContext saved by the DataManager.

--- halfcaff/src/com/ualberta/team17/datamanager/test/DataManagerTest.java
+++ halfcaff/src/com/ualberta/team17/datamanager/test/DataManagerTest.java
@@ -89,14 +89,14 @@ public class DataManagerTest extends ActivityInstrumentationTestCase2<QuestionLi
 		super(QuestionListActivity.class);
 	}
 	
-	public static final String TEST_DATA = "[[\"Question\",{\"id\":\"c4ca4238a0b92382dcc509a6f75849b\",\"parent\":\"0\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testBody\",\"title\":\"testTitle\"}],[\"Answer\",{\"id\":\"c81e728d9d4c2f636f67f89cc14862c\",\"parent\":\"c4ca4238a0b92382dcc509a6f75849b\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testReply\"}],[\"Answer\",{\"id\":\"eccbc87e4b5ce2fe28308fd9f2a7baf3\",\"parent\":\"c4ca4238a0b92382dcc509a6f75849b\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testReply2\"}],[\"Comment\",{\"id\":\"a87ff679a2f3e71d9181a67b7542122c\",\"parent\":\"c81e728d9d4c2f636f67f89cc14862c\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"comment!\"}]]";
+	public static final String TEST_DATA = "[[\"Question\",{\"id\":\"c4ca4238a0b923820dcc509a6f75849b\",\"type\":\"question\",\"parent\":\"0\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testBody\",\"title\":\"testTitle\"}],[\"Answer\",{\"id\":\"c81e728d9d4c2f636f067f89cc14862c\",\"type\":\"answer\",\"parent\":\"c4ca4238a0b923820dcc509a6f75849b\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testReply\"}],[\"Answer\",{\"id\":\"eccbc87e4b5ce2fe28308fd9f2a7baf3\",\"type\":\"answer\",\"parent\":\"c4ca4238a0b923820dcc509a6f75849b\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"testReply2\"}],[\"Comment\",{\"id\":\"a87ff679a2f3e71d9181a67b7542122c\",\"type\":\"comment\",\"parent\":\"c81e728d9d4c2f636f067f89cc14862c\",\"author\":\"test_user\",\"date\":\"1970-01-01T00:00:00Z\",\"body\":\"comment!\"}]]";
 	
 	public void test_DataManager() {
 		// User context
 		userContext = new UserContext("test_user");
 		
 		// Make local data manager, and set the data to the test data set
-		dataManager = new LocalDataManager(getActivity(), userContext);
+		dataManager = new LocalDataManager(getActivity());
 		dataManager.writeTestData(TEST_DATA);
 		
 		// Make net data manager
@@ -110,9 +110,6 @@ public class DataManagerTest extends ActivityInstrumentationTestCase2<QuestionLi
 		
 		// Make the controller
 		controller = new QAController(manager);
-		
-		// Wait for the local data to be loaded
-		dataManager.waitForData();
 		
 		// Do a query
 		DataFilter f = new DataFilter();
