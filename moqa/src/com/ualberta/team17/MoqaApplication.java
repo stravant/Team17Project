3ffcf7ee50a00a75dff7e639e06bc9d648852803
Mobius5150 <mike@michaelblouin.ca>
2014-11-05 20:57:43 -0700

    Initializing data manager stuff on application load.

---
+++ moqa/src/com/ualberta/team17/MoqaApplication.java
@@ -0,0 +1,32 @@
+package com.ualberta.team17;
+
+import com.ualberta.team17.controller.QAController;
+import com.ualberta.team17.datamanager.DataManager;
+import com.ualberta.team17.datamanager.LocalDataManager;
+import com.ualberta.team17.datamanager.NetworkDataManager;
+import com.ualberta.team17.datamanager.UserContext;
+
+import android.app.Application;
+import android.content.res.Resources;
+
+public class MoqaApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Make local data manager, and set the data to the test data set
+		LocalDataManager localDataManager = new LocalDataManager(getApplicationContext(), new UserContext());
+		
+		// Make net data manager
+		Resources resources = getResources();
+		NetworkDataManager netDataManager = new NetworkDataManager(
+				resources.getString(R.string.esTestServer),
+				resources.getString(R.string.esTestIndex));
+		
+		// Make the manager
+		DataManager dataManager = new DataManager(getApplicationContext(), localDataManager, netDataManager);
+
+		// Finally instantiate the controller
+		new QAController(dataManager);
+	}
+}
