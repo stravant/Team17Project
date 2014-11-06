aacd9aa29fbd1d4dc642b13a321ddbced0b0163d
j-rewerts <rewerts@ualberta.ca>
2014-11-06 12:21:26 -0700

    Added the login activity

---
+++ moqa/src/com/ualberta/team17/view/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.ualberta.team17.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.ualberta.team17.R;
+import com.ualberta.team17.controller.QAController;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		TextView tv = (TextView) findViewById(R.id.loginWarning);
+		if (tv != null) {
+			tv.setVisibility(View.INVISIBLE);
+		}
+		
+		Button loginButton = (Button) findViewById(R.id.signInButton);
+		if (loginButton != null) {
+			loginButton.setOnClickListener( new OnClickListener() {
+
+	            @Override
+	            public void onClick(View v) {
+	                LoginActivity.this.login();
+	            }
+	        });
+		}
+	}
+	
+	private void login() {
+		String username;
+		
+		EditText usernameET = (EditText) findViewById(R.id.usernameText);
+		if (usernameET != null) {
+			username = usernameET.getText().toString();
+			
+			if (username.length() >= 6 && username.length() <= 20 && !username.contains(" ")) {
+				// Create the user context.
+				//QAController.getInstance().
+			}
+			else {
+				TextView tv = (TextView) findViewById(R.id.loginWarning);
+				if (tv != null) {
+					tv.setVisibility(View.VISIBLE);
+				}
+			}
+		}
+	}
+}
