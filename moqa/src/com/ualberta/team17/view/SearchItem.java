3e18a74b5b506976f9526bc4a7f6685fdb2cad99
j-rewerts <rewerts@ualberta.ca>
2014-11-18 20:56:10 -0700

    Added some icons. Very basic actionbar functionality.

--- moqa/src/com/ualberta/team17/view/SearchItem.java
+++ moqa/src/com/ualberta/team17/view/SearchItem.java
@@ -1,6 +1,7 @@
 package com.ualberta.team17.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
