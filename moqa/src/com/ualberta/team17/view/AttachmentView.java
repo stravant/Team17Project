0059961c3fd0aaff33f8e72931dbc6c90302e631
Mobius5150 <mike@michaelblouin.ca>
2014-11-22 20:20:22 -0700

    Adding full screen display of attachment on click.

--- moqa/src/com/ualberta/team17/view/AttachmentView.java
+++ moqa/src/com/ualberta/team17/view/AttachmentView.java
@@ -5,12 +5,21 @@ import java.util.List;
 
 import com.ualberta.team17.AttachmentItem;
 import com.ualberta.team17.QAModel;
+import com.ualberta.team17.R;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 
 /**
@@ -62,6 +71,8 @@ public class AttachmentView extends HorizontalScrollView {
 							(int)(image.getWidth() * ((float)this.getHeight())/image.getHeight()),
 							LayoutParams.MATCH_PARENT));
 
+			imageView.setOnClickListener(new ImageViewOnClickListener());
+			
 			baseLayout.addView(imageView);
 			mImageViews.add(imageView);
 		}
@@ -93,6 +104,44 @@ public class AttachmentView extends HorizontalScrollView {
 					new LinearLayout.LayoutParams(
 							(int)(image.getWidth() * ((float)this.getHeight())/image.getHeight()),
 							LayoutParams.MATCH_PARENT));
+		}
+	}
+
+	private class ImageViewOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View arg0) {
+			Context baseContext = AttachmentView.this.getContext();
+			if (baseContext instanceof Activity) {
+				ImageViewDialog imd = new ImageViewDialog(mAttachments.get(mImageViews.indexOf(arg0)));
+				imd.show(((Activity)baseContext).getFragmentManager(), "Attachment Dialog");
+			}
+		}
+	}
+
+	private class ImageViewDialog extends DialogFragment {
+		AttachmentItem mBaseItem;
+		ImageView baseView;
+
+		public ImageViewDialog(AttachmentItem baseItem) {
+			mBaseItem = baseItem;
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+		    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		    LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		    View v = inflater.inflate(R.layout.image_view_dialog, null, false);
+		    builder.setView(v);
+		    baseView = (ImageView) v.findViewById(R.id.mainImageView);
+		    baseView.setImageBitmap(mBaseItem.getImage());
+		    baseView.setScaleType(ScaleType.FIT_CENTER);
+		    Dialog d = builder.create();
+		    d.getWindow().setFlags(
+		    		android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		    		android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		    
+		    return d;
 		}
 	}
 }
